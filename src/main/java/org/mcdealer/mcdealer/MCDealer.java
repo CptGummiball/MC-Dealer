c30cfeed9978660db71e694e0329404a5e829aea
CptGummiball <treeman1992@outlook.de>
2023-12-12 22:58:34 +0100

    - Added pluginEnabled checker - Added scheduler 5 minutes

--- src/main/java/org/mcdealer/mcdealer/MCDealer.java
+++ src/main/java/org/mcdealer/mcdealer/MCDealer.java
@@ -1,37 +1,44 @@
 package org.mcdealer.mcdealer;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class MCDealer extends JavaPlugin {
+    private boolean pluginEnabled = false;
 
     @Override
     public void onEnable() {
 
+        pluginEnabled = true;
+
         // Extracts necessary resources from the JAR file
-        this.getLogger().info(" [MCDealer] Check resources... ");
+        this.getLogger().info(" Check resources... ");
         new ResourceUpdater(this).updateConfig();
         new ResourceUpdater(this).updatePyScript();
         new ResourceUpdater(this).updateWebFolder();
 
         // Run Webserver
         new ConfigUpdater(this).webConfigUpdater();
-        this.getLogger().info(" [MCDealer] Starting Webserver ");
+        this.getLogger().info(" Starting Webserver ");
         WebServer webServer = new WebServer(this);
         webServer.RunWebServer();
 
         this.getLogger().info(" [MCDealer] by CptGummiball and Vollmondheuler enabled! ");
 
-        // Schedule the recurring task based on the interval
-        new TaskScheduler(this).loadConfig();
-        int delay = 300;
-        getServer().getScheduler().runTaskLater(this, new TaskScheduler(this).scheduleRepeatingTask(), delay);
-        new TaskScheduler(this).scheduleRepeatingTask();
+        // Schedule the recurring task with a delay after the server has loaded
+        Bukkit.getScheduler().runTaskLater(this, () -> {
+            if (pluginEnabled) {
+                new TaskScheduler(this).loadConfig();
+                new TaskScheduler(this).scheduleRepeatingTask();
+            }
+        }, 20L); // 20 ticks delay (1 second = 20 ticks)
     }
 
     @Override
     public void onDisable() {
 
         new WebServer(this).stopWebServer();
+        pluginEnabled = false;
         this.getLogger().info(" [MCDealer] by CptGummiball and Vollmondheuler disabled! ");
 
     }
