836e0c53d877fb699732a7594b5484a6f2797064
CptGummiball <treeman1992@outlook.de>
2023-12-13 20:05:31 +0100

    - added reload command class

---
+++ src/main/java/org/mcdealer/mcdealer/commands/Reload.java
@@ -0,0 +1,33 @@
+package org.mcdealer.mcdealer.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.mcdealer.mcdealer.MCDealer;
+
+public class Reload implements CommandExecutor {
+
+    private final MCDealer plugin;
+
+    public Reload (MCDealer plugin) {
+        this.plugin = plugin;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (label.equalsIgnoreCase("reload")) {
+            // Überprüfe, ob der Absender die erforderlichen Berechtigungen hat (falls erforderlich)
+            if (!sender.hasPermission("mcdealer.reload")) {
+                sender.sendMessage("You do not have the required permissions to run this command.");
+                return true;
+            }
+
+            // Führe den Befehl aus
+            plugin.ConfigUpdater.webConfigUpdater();
+            plugin.WebServer.restartWebServer();
+            sender.sendMessage("Webserver restarted");
+            return true;
+        }
+        return false;
+    }
+}
