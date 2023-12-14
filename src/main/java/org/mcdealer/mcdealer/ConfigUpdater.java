e53e725ce8272247f65ac4642af541a8cde04973
CptGummiball <treeman1992@outlook.de>
2023-12-14 11:04:48 +0100

    - redone webconfig updater

--- src/main/java/org/mcdealer/mcdealer/ConfigUpdater.java
+++ src/main/java/org/mcdealer/mcdealer/ConfigUpdater.java
@@ -2,12 +2,16 @@
 package org.mcdealer.mcdealer;
 
 import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.configuration.file.YamlConfiguration;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class ConfigUpdater {
@@ -22,7 +26,7 @@ public class ConfigUpdater {
 
     public void webConfigUpdater() {
         // Load the configurations
-        FileConfiguration jsonConfig = loadJsonConfig();
+        JSONObject jsonConfig = loadJsonConfig();
         FileConfiguration yamlConfig = getConfig();
 
         // Synchronize the values of the configurations
@@ -32,24 +36,31 @@ public class ConfigUpdater {
         saveJsonConfig(jsonConfig);
     }
 
-    private FileConfiguration loadJsonConfig() {
+    private JSONObject loadJsonConfig() {
         File configFile = new File(plugin.getDataFolder(), "/web/assets/config.json");
-        return YamlConfiguration.loadConfiguration(configFile);
-    }
 
-    private void syncValues(FileConfiguration yamlConfig, FileConfiguration jsonConfig) {
-        // Synchronize the values
-        jsonConfig.set("currencySymbol", yamlConfig.getString("currencySymbol"));
-        jsonConfig.set("currencySymbolPosition", yamlConfig.getString("currencySymbolPosition"));
-        jsonConfig.set("defaultLanguage", yamlConfig.getString("defaultLanguage"));
-    }
-
-    private void saveJsonConfig(FileConfiguration jsonConfig) {
-        File configFile = new File(plugin.getDataFolder(), "/web/assets/config.json");
-        try {
-            jsonConfig.save(configFile);
+        try (FileReader reader = new FileReader(configFile)) {
+            return new JSONObject(new JSONTokener(reader));
         } catch (IOException e) {
-            logger.warn(" Json could not be saved ");
+            logger.warn("Json could not be loaded", e);
+            return new JSONObject();
+        }
+    }
+
+    private void syncValues(FileConfiguration yamlConfig, JSONObject jsonObj) {
+        // Synchronize the values
+        jsonObj.put("currencySymbol", yamlConfig.getString("currencySymbol"));
+        jsonObj.put("currencySymbolPosition", yamlConfig.getString("currencySymbolPosition"));
+        jsonObj.put("defaultLanguage", yamlConfig.getString("defaultLanguage"));
+    }
+
+    private void saveJsonConfig(JSONObject jsonObj) {
+        File configFile = new File(plugin.getDataFolder(), "/web/assets/config.json");
+
+        try (FileWriter writer = new FileWriter(configFile)) {
+            jsonObj.write(writer);
+        } catch (IOException e) {
+            logger.warn("Json could not be saved", e);
         }
     }
 
