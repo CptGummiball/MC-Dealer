6df8a3a11102c8bac98b2dfdc789f0f89df4d031
CptGummiball <treeman1992@outlook.de>
2023-12-15 10:36:12 +0100

    - replaced java HTTP with Jetty

--- src/main/java/org/mcdealer/mcdealer/MCDealer.java
+++ src/main/java/org/mcdealer/mcdealer/MCDealer.java
@@ -1,14 +1,17 @@
 package org.mcdealer.mcdealer;
 
+import org.bukkit.ChatColor;
 import org.mcdealer.mcdealer.Utils.DataMangager.ResourceUpdater;
 import org.mcdealer.mcdealer.Utils.DataMangager.ConfigUpdater;
-import org.mcdealer.mcdealer.Utils.HTTP.WebServer;
+import org.mcdealer.mcdealer.Utils.HTTP.JettyServer;
 
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.logging.Level;
 
 public class MCDealer extends JavaPlugin {
 
@@ -26,24 +29,28 @@ public class MCDealer extends JavaPlugin {
         // Extracts necessary resources from the JAR file
         new ResourceUpdater(this).updateWebFolder();
         new ResourceUpdater(this).updateConfig();
-
         // Run Webserver
-        new ConfigUpdater(this).webConfigUpdater();
         logger.info("Starting Webserver");
-        WebServer webServer = new WebServer(this);
-        webServer.runWebServer();
+        setNewWebSettings();
+        JettyStart();
         logger.info("[MCDealer] by CptGummiball and Vollmondheuler enabled!");
-
         // Initialize the scheduler
         loadConfig();
         initScheduler();
+    }
+
+    @Override
+    public void onDisable() {
+
+        pluginEnabled = false;
+        JettyStop();
+        logger.info("[MCDealer] by CptGummiball and Vollmondheuler disabled!");
     }
 
     private void loadConfig() {
         // Load config for UpdateInterval
         getConfig().options().copyDefaults(true);
         saveConfig();
-
         // Read the value of UpdateInterval from the configuration
         UpdateInterval = getConfig().getInt("UpdateInterval", 300);
     }
@@ -68,12 +75,20 @@ public class MCDealer extends JavaPlugin {
         new ConfigUpdater(this).webConfigUpdater();
     }
 
-    @Override
-    public void onDisable() {
+    public void JettyStart() {
+        int port = getConfig().getInt("webServerPort", 8080);
+        try {
+            JettyServer.start(port);
+        } catch (Exception e) {
+            logger.error("Failed to start Jetty Server!");
+        }
+    }
 
-        new WebServer(this).stopWebServer();
-        pluginEnabled = false;
-        logger.info("[MCDealer] by CptGummiball and Vollmondheuler disabled!");
-
+    public void JettyStop() {
+        try {
+            JettyServer.stop();
+        } catch (Exception e) {
+            logger.error("Failed to stop Jetty Server!");
+        }
     }
 }
