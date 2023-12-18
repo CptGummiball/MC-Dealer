fc0aa1189a8672950cc435ec4f77d590f298a958
CptGummiball <treeman1992@outlook.de>
2023-12-18 07:41:44 +0100

    - added command and suffixes class

--- src/main/java/org/mcdealer/mcdealer/MCDealer.java
+++ src/main/java/org/mcdealer/mcdealer/MCDealer.java
@@ -1,36 +1,50 @@
 package org.mcdealer.mcdealer;
 
-import org.mcdealer.mcdealer.Utils.DataMangager.ResourceUpdater;
-import org.mcdealer.mcdealer.Utils.DataMangager.ConfigUpdater;
-import org.mcdealer.mcdealer.Utils.HTTP.JettyServer;
-
+import org.mcdealer.mcdealer.Utils.HTTP.WebServerManager;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 
+import org.mcdealer.mcdealer.Utils.DataMangager.ResourceUpdater;
+import org.mcdealer.mcdealer.Utils.DataMangager.ConfigUpdater;
 import org.mcdealer.mcdealer.Utils.JythonScriptRunner;
+import org.mcdealer.mcdealer.Utils.ShopHandler;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class MCDealer extends JavaPlugin {
 
     private static final Logger logger = LoggerFactory.getLogger("MCDealer");
-
+    private WebServerManager webServerManager;
+    private ConfigUpdater configUpdater;
     private boolean pluginEnabled = false;
     int delayTicks = 2400;
     protected int UpdateInterval;
+
 
     @Override
     public void onEnable() {
 
         pluginEnabled = true;
+        ShopHandler ShopHandler = new ShopHandler(this);
+        getServer().getPluginManager().registerEvents(ShopHandler, this);
+
 
         // Extracts necessary resources from the JAR file
         new ResourceUpdater(this).updateWebFolder();
         new ResourceUpdater(this).updateConfig();
         // Run Webserver
         logger.info("Starting Webserver");
-        setNewWebSettings();
-        JettyStart();
+        configUpdater = new ConfigUpdater(this);
+        configUpdater.webConfigUpdater();
+        webServerManager = new WebServerManager(this);
+        webServerManager.JettyStart();
+        // Register Commands
+        getCommand("mcdealer").setExecutor(new MCDealerCommand());
         logger.info("[MCDealer] by CptGummiball and Vollmondheuler enabled!");
         // Initialize the scheduler
         loadConfig();
@@ -41,7 +55,7 @@ public class MCDealer extends JavaPlugin {
     public void onDisable() {
 
         pluginEnabled = false;
-        JettyStop();
+        webServerManager.JettyStop();
         logger.info("[MCDealer] by CptGummiball and Vollmondheuler disabled!");
     }
 
@@ -60,7 +74,7 @@ public class MCDealer extends JavaPlugin {
                 if (pluginEnabled) {
                     try {
                         loadConfig();
-                        setNewWebSettings();
+                        configUpdater.webConfigUpdater();
                         JythonScriptRunner.runPythonScript();
                         logger.info("Shopdata updated!");
                     } catch (Exception e) {
@@ -71,24 +85,56 @@ public class MCDealer extends JavaPlugin {
         }.runTaskTimer(this, delayTicks, UpdateInterval);
     }
 
-    public void setNewWebSettings() {
-        new ConfigUpdater(this).webConfigUpdater();
-    }
+    private class MCDealerCommand implements CommandExecutor {
+        @Override
+        public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+            if (!(sender instanceof Player)) {
+                sender.sendMessage("Dieser Befehl kann nur von Spielern verwendet werden.");
+                return true;
+            }
 
-    public void JettyStart() {
-        int port = getConfig().getInt("webServerPort", 8080);
-        try {
-            JettyServer.start(port);
-        } catch (Exception e) {
-            logger.error("Failed to start Jetty Server!");
-        }
-    }
+            Player player = (Player) sender;
 
-    public void JettyStop() {
-        try {
-            JettyServer.stop();
-        } catch (Exception e) {
-            logger.error("Failed to stop Jetty Server!");
+            // Überprüfe die Hauptberechtigung
+            if (!player.hasPermission("mcdealer.use")) {
+                player.sendMessage("Du hast keine Berechtigung für diesen Befehl.");
+                return true;
+            }
+
+            if (args.length == 0) {
+                player.sendMessage("Verwendung: /mcdealer <getshopuuid|removeshopuuid>");
+                return true;
+            }
+
+            // Überprüfe das erste Argument (Suffix) und rufe die entsprechende Methode auf
+            switch (args[0].toLowerCase()) {
+                case "hideshop":
+                    if (player.hasPermission("mcdealer.hideshop")) {
+                        shopHandler.handleGetShopUUID(player);
+                    } else {
+                        player.sendMessage("Du hast keine Berechtigung, Shop-UUIDs abzurufen.");
+                    }
+                    break;
+                case "showshop":
+                    if (player.hasPermission("mcdealer.showshop")) {
+                        shopHandler.handleRemoveShopUUID(player);
+                    } else {
+                        player.sendMessage("Du hast keine Berechtigung, Shop-UUIDs zu entfernen.");
+                    }
+                    break;
+                case "restart":
+                    if (player.hasPermission("mcdealer.restart")) {
+                        webServerManager.JettyRestart();
+                    } else {
+                        player.sendMessage("Du hast keine Berechtigung, Shop-UUIDs zu entfernen.");
+                    }
+                    break;
+                default:
+                    player.sendMessage("Unbekanntes Argument: " + args[0]);
+                    break;
+            }
+
+            return true;
         }
     }
 }
